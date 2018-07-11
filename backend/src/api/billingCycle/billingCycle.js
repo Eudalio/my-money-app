94b14e013c7d47da2d92a9b6981422a744db21e6
eudalio <tonnydallios@gmail.com>
2018-07-11 19:03:13 -0300

    API - Backend Finalizado

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -3,13 +3,13 @@ const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, required: true }
+    value: { type: Number, min: 0, required: true }
 })
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min:0, required: [true, 'Informe o valor do débito!'] },
-    status: { type: String, required: false, uppercase: true, 
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
+    status: { type: String, required: false, uppercase: true,
         enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
 
