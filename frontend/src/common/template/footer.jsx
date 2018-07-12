609b1f2f83cf645a27c3a02d3874cc7b2754c323
eudalio <tonnydallios@gmail.com>
2018-07-11 22:24:21 -0300

    Componente Rodapé

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2018
+            <a href="http://www.github.com/eudalio" target="_blank"> Eudálio Sousa </a>
+        </strong>
+    </footer>
+)
